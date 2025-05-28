ce8af1f8fbb7dc18fe6b511861ed18a9a7c17f96
oli.oil <princessolingay@gmail.com>
2025-05-28 21:45:45 +0800

    simple book api with mongodb out of branch done

--- 07_laboratory_url_module.js
+++ 07_laboratory_url_module.js
@@ -1,7 +1,26 @@
+// const url = require('url');
+
+// const myUrl = 'http://www.example.com:8080/pathname?name=JohnDoe#fragment';
+// const parsedUrl = url.parse(myUrl, true);
+
+// console.log(parsedUrl);
+// console.log(parsedUrl.query.name);
+
 const url = require('url');
 
-const myUrl = 'http://www.example.com:8080/pathname?name=JohnDoe#fragment';
-const parsedUrl = url.parse(myUrl, true);
+const urlString = 'https:                                                                 
 
-console.log(parsedUrl);
-console.log(parsedUrl.query.name);
+const parsedUrl = url.parse(urlString, true);
+
+console.log('//www.example.com:8080/products?category=electronics&sort=price';
+
+const parsedUrl = url.parse(urlString, true);
+
+console.log('URL Components:');
+console.log(Protocol: ${parsedUrl.protocol});
+console.log(Hostname: ${parsedUrl.hostname});
+console.log(Port: ${parsedUrl.port});
+console.log(Pathname: ${parsedUrl.pathname});
+console.log(Query Parameters: ${parsedUrl.query});
+console.log(Query Parameters (as object):);
+console.log(parsedUrl.query);
